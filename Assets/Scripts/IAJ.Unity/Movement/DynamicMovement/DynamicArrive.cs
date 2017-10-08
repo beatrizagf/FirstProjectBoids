cb01cbef832b02f53dd0d8ea4ca2429dccee7bc6
Pedro Fernandes <jelkzul@gmail.com>
2017-10-08 10:56:28 +0100

    Add DynamicArrive from lab2

---
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicArrive.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement {
+	public class DynamicArrive : DynamicVelocityMatch {
+		public override string Name {
+			get { return "Arrive"; }
+		}
+
+		public float MaxSpeed { get; set; }
+		float StopRadius { get; set; }
+		float SlowRadius { get; set; }
+		public KinematicData RealTarget { get; set; }
+
+		public DynamicArrive() {
+			//FakeTarget = new KinematicData();
+			StopRadius = 3.0f;
+			SlowRadius = 30.0f;
+		}
+
+		public override MovementOutput GetMovement() {
+			float targetSpeed;
+			
+
+			Vector3 direction = Target.Position - Character.Position;
+			float distance = direction.magnitude;
+
+			if(distance < StopRadius) {
+				targetSpeed = 0;
+			} else if (distance > SlowRadius) {
+				targetSpeed = MaxSpeed;
+			} else {
+				targetSpeed = MaxSpeed * (distance / SlowRadius);
+			}
+
+			Target.velocity = direction.normalized * targetSpeed;
+
+
+			return base.GetMovement();
+		}
+	}
+}
